21e0cd6a9faafe4e85f8d40c3576b9430ff139ef
Eric Batiste <ericbatiste4@gmail.com>
2024-01-05 16:42:13 -0700

    Get users saved recipes re-render on page refresh.

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,34 +1,14 @@
-export const getPromises = [
-  fetch('http://localhost:3001/api/v1/recipes')
-  .then(res => {
-    if(!res.ok) {
-      throw new Error()
-    }
-    return res.json();
-  }),
-  fetch('http://localhost:3001/api/v1/ingredients')
-  .then(res => {
-    if(!res.ok) {
-      throw new Error()
-    }
-    return res.json()
-  }),
+const getPromises = [
+  fetch('http://localhost:3001/api/v1/recipes'),
+  fetch('http://localhost:3001/api/v1/ingredients'),
   fetch('http://localhost:3001/api/v1/users')
-  .then(res => {
-    if(!res.ok) {
-      throw new Error()
-    }
-    return res.json()
-  }),
-  // fetch('http://localhost:3001/api/v1/usersRecipes')
-  // .then(res => {
-  //   if(!res.ok) {
-  //     throw new Error()
-  //   }
-  //   return res.json()
-  // })
 ];
 
 export const postUrl = 'http://localhost:3001/api/v1/usersRecipes';
+
+export const fetchData = () => {
+  return Promise.all(getPromises);
+}
+
 
 
